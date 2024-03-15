049efd24002f3b7d26210dfda7fa462e616ab861
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-03-15 21:08:10 +0400

    Static and media directories

--- landingpagewebsite_root/landingpagewebsite/urls.py
+++ landingpagewebsite_root/landingpagewebsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from crm import views
@@ -23,4 +25,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.first_page),
     path("thanks/", views.thanks_page, name="thanks_page"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
